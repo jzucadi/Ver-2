e4c6da64ea2d159d3f1ac775dd6352127f46944f
James Zaccardo <jameszaccardo@gmail.com>
2017-07-24 02:43:08 -0700

    poking

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
         Hi. I'm James Zaccardo, a frontend developer based in Los Angeles.
       </h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="#" target="_blank" class="button--green">View Site</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
