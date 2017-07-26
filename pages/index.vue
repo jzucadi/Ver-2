599629f3a1c0f031fd819cf81f7d52aaddec6a43
James Zaccardo <jameszaccardo@gmail.com>
2017-07-25 23:35:06 -0700

    adding GSAP animations

--- pages/index.vue
+++ pages/index.vue
@@ -21,11 +21,35 @@
 </template>
 
 <script>
-import Logo from '~components/Logo.vue'
+import { TweenMax, Back } from 'gsap'
 
 export default {
-  components: {
-    Logo
+  transition: {
+    mode: 'out-in',
+    css: false,
+    beforeEnter (el) {
+      TweenMax.set(el, {
+        transformPerspective: 600,
+        perspective: 300,
+        transformStyle: 'preserve-3d'
+      })
+    },
+    enter (el, done) {
+      TweenMax.to(el, 1, {
+        rotationY: 360,
+        transformOrigin: '50% 50%',
+        ease: Back.easeOut
+      })
+      done()
+    },
+    leave (el, done) {
+      TweenMax.to(el, 1, {
+        rotationY: 0,
+        transformOrigin: '50% 50%',
+        ease: Back.easeIn
+      })
+      done()
+    }
   }
 }
 </script>
