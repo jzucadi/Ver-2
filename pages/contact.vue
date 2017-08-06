25b2f57f02fca996f106cae278040000511c1667
James Zaccardo <jameszaccardo@gmail.com>
2017-08-05 20:52:12 -0700

    changes

---
+++ pages/contact.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="container">
+      <nuxt-link class="navi" to="/">
+            Home
+          </nuxt-link>
+    <h1 class="title">
+      Let's get in Touch</h1>
+      <div class="email"><a href="mailto:jameszaccardo@gmail.com?Subject=Hello" target="_top">jameszaccardo@gmail.com</a></div>
+  
+  
+
+   
+        
+  </section>
+</template>
+<style>
+:root {
+    --color-main: #208FF9;
+  }
+
+body {
+  color: var(--color-main);
+}
+</style>
--- pages/work.vue
+++ pages/work.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="container">
       <nuxt-link class="navi" to="/">
-            Home
+           Work
           </nuxt-link>
     <h1 class="title">
-      Let's get in Touch</h1>
-      <div class="email"><a href="mailto:jameszaccardo@gmail.com?Subject=Hello" target="_top">jameszaccardo@gmail.com</a></div>
+      stuff</h1>
+      
   
   
 
