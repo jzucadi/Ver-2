3f7b8018ee47a1c780d463da38a9a7a6d69c6dd1
James Zaccardo <jameszaccardo@gmail.com>
2017-08-05 20:31:51 -0700

    added autoprefixer support

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,10 @@ module.exports = {
   */
   build: {
     postcss: [
-      require('lost')
+      require('lost'),
+      require('autoprefixer')({
+        browsers: ['last 2 versions', 'Firefox ESR', '> 1%', 'ie >= 8', 'iOS >= 8', 'Android >= 4']
+      }), 
     ],
     /*
     ** Run ESLINT on save
