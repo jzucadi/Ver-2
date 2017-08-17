896967bcb81dfd4c97022599e0f6afae57badb5d
James Zaccardo <jameszaccardo@gmail.com>
2017-08-16 21:57:29 -0700

    testing lost grid

---
+++ pages/showcase.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="container">
+      <nuxt-link class="navi" to="/">
+           Work
+          </nuxt-link>
+    <h1 class="title">
+      stuff</h1>
+      
+      
+  
+  
+
+   
+        
+  </section>
+</template>
+<script>
+import WOW from 'wowjs'
+if (process.BROWSER_BUILD) {
+  require('wowjs')
+}
+export default {
+  
+WOW().init()
+}
+
+</script>
+<style>
+@lost gutter 20px;
+@lost flexbox flex;
+</style>
